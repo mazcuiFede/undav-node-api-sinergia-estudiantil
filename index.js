0b2d7d24ae69128b7890dc5f26e45d4b0c9976d6
Matias Azcui <matias.azcui@gmail.com>
2022-01-12 22:31:10 -0300

    ya casi terminada, falta refactor pero la funcionalidad está

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const app = express()
 const port = process.env.PORT || 3002
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
@@ -23,9 +31,6 @@ mongoose.connect(process.env.CONN_STRING, (err, res) => {
     console.log("Conexión a database establecida")
 })
 
-app.get("/api/user", (req, res) => {
-
-})
 
 app.post("/api/user", (req, res) => {
     
@@ -44,6 +49,18 @@ app.post("/api/user", (req, res) => {
 
         res.status(201).send({user})
     })
+})
+
+app.get("/api/evento", (req, res) => {
+    console.log("GET /api/evento")
+    
+
+    Evento.find({}, (err, evento) => {
+        if (err) res.status(500).send({message: "hubo un error al obtener los eventos"})
+        if (!evento) return res.status(404).send({message: "no hay eventos"})
+        res.status(200).send({evento})
+    })
+
 })
 
 app.post("/api/evento", (req, res) => {
@@ -66,14 +83,53 @@ app.post("/api/evento", (req, res) => {
 })
 
 
-app.get("/api/duda", (req, res) => {
-    console.log("POST /api/duda")
+app.get("/api/duda/:id", (req, res) => {
+    console.log("GET /api/duda/:id")
     
-    let dudaId = req.params.dudaId
+    let id = req.params.id
 
-    Duda.findById(dudaId, (err, duda) => {
+    Duda.findById(id, (err, duda) => {
         if (err) res.status(500).send({message: "hubo un error al obtener la duda"})
-        if (!product) return res.status(404).send({message: "duda no encontrada"})
+        if (!duda) return res.status(404).send({message: "duda no encontrada"})
+        res.status(200).send({duda})
+    })
+})
+
+app.get("/api/duda/tipo/:tipo", (req, res) => {
+    console.log("GET /api/duda/:tipo")
+    
+    let tipo = req.params.tipo
+
+    Duda.find({tipo}, (err, duda) => {
+        if (err) res.status(500).send({message: "hubo un error al obtener la duda"})
+        if (!duda) return res.status(404).send({message: "duda no encontrada"})
+        res.status(200).send({duda})
+    })
+
+})
+
+app.put("/api/duda/:id", (req, res) => {
+    console.log("PUT /api/duda/:id")
+    
+    let id = req.params.id
+    let body = req.body
+
+    Duda.findByIdAndUpdate(id, body, (err, dudaUpdated) => {
+        if (err) res.status(500).send({message: "hubo un error al obtener la duda"})
+        if (!dudaUpdated) return res.status(404).send({message: "duda no encontrada"})
+
+        console.log(dudaUpdated)
+        res.status(200).send({duda : dudaUpdated})
+    })
+
+})
+
+app.get("/api/duda", (req, res) => {
+    console.log("GET /api/duda")
+    
+    Duda.find({}, (err, duda) => {
+        if (err) res.status(500).send({message: "hubo un error al obtener las dudas"})
+        if (!duda) return res.status(404).send({message: "no hay dudas"})
 
         res.status(200).send({duda})
     })
@@ -99,6 +155,26 @@ app.post("/api/duda", (req, res) => {
     })
     
 })
+
+// app.put("/api/duda", (req, res) => {
+    
+//     console.log("POST /api/duda")
+//     console.log(req.body)
+
+//     let duda = new Duda()
+//     duda.titulo = req.body.titulo
+//     duda.descripcion = req.body.descripcion
+//     duda.puntos = 0
+//     duda.tags = req.body.tags
+//     duda.tipo = req.body.tipo
+
+//     duda.save((err, duda) => {
+//         if (err) res.status(500).send({message: "hubo un error al guardar la duda"})
+
+//         res.status(201).send({duda})
+//     })
+    
+// })
 
 
 app.listen(port, () => {
